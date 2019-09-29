b73f83f1dfe3b4e8809883c2ce40b096af0a0898
vasylw <vasylw@gmail.com>
2019-09-29 11:27:02 +0300

    First commit

---
+++ Pipelines/JenkinsfileSSH_Pub_plugin.groovy
@@ -0,0 +1,70 @@
+
+pipeline {
+    agent any
+    stages {
+        stage('Preparation') {
+            steps {
+                    git 'https://github.com/microservices-demo/carts'
+            }
+        }
+    stage('Build') {
+            steps {
+                sh 'mvn clean package' 
+                
+            }
+        }
+    stage('Test') {
+            steps {
+                sh 'mvn test' 
+                
+            }
+        }
+    stage('Archive') {
+            steps {
+                archiveArtifacts artifacts: 'target/*.*', fingerprint: true
+            }
+    }
+    
+    
+    
+    
+    
+    stage('Deploy') {
+        steps {
+        sshPublisher(publishers: [sshPublisherDesc(configName: 'EC2_Linux_CI_Server',
+        transfers: [sshTransfer(cleanRemote: true, excludes: '', 
+        execCommand: '', 
+        execTimeout: 120000, 
+        flatten: false, 
+        makeEmptyDirs: false, 
+        noDefaultExcludes: false, 
+        patternSeparator: '[, ]+', 
+        remoteDirectory: 'carts/', 
+        remoteDirectorySDF: true, 
+        removePrefix: '', 
+        sourceFiles: '/var/lib/jenkins/workspace/carts/target/carts.jar')], 
+        usePromotionTimestamp: true, 
+        useWorkspaceInPromotion: false, 
+        verbose: false)])
+        }
+    }
+    }
+    post {
+        always {
+            echo 'I have finished and deleting workspace'
+            deleteDir() 
+        }
+        success {
+            echo 'Job succeeeded!'
+        }
+        unstable {
+            echo 'I am unstable :/'
+        }
+        failure {
+            echo 'I failed :('
+        }
+        changed {
+            echo 'Things were different before...'
+        }
+    }
+}
